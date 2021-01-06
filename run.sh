aeecc466c7f57244be142b114adef53a8255a892
nverno <noah.v.peart@gmail.com>
2021-01-06 07:52:22 -0800

    add little pre-flight check

--- run.sh
+++ run.sh
@@ -10,4 +10,12 @@ if [ -f "${DIR}/.env" ]; then
     . "$DIR/.env"
 fi
 
+if [ -z "$GITHUB_USERNAME" ]; then
+    echo "Must define GITHUB_USERNAME and GITHUB_PASSWORD" && exit 1
+fi
+
+if ! command -v chromedriver >/dev/null 2>&1; then
+    echo "chromedriver not found" && exit 1
+fi
+
 python automator.py

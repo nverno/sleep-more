cca5dad3b858a82eff57106a8ad8202bf062d527
nverno <noah.v.peart@gmail.com>
2021-01-06 07:44:59 -0800

    squash

--- automator.py
+++ automator.py
@@ -2,7 +2,6 @@
 
 import os
 from selenium import webdriver
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
